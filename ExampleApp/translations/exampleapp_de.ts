b751d26fd4195f594d0dc8d032b8a372c14ca9aa
Sylvain Colinet <scolinet@gmail.com>
2023-06-28 11:15:15 +0200

    ExampleApp: add translation files

---
+++ ExampleApp/translations/exampleapp_de.ts
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>ExampleApp</name>
+    <message>
+        <location filename="../exampleapp.ui" line="14"/>
+        <source>ExampleApp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../exampleapp.ui" line="27"/>
+        <source>Debug Log Path : </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../exampleapp.ui" line="40"/>
+        <source>Info Log Path :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../exampleapp.ui" line="53"/>
+        <location filename="../exampleapp.ui" line="69"/>
+        <location filename="../exampleapp.ui" line="85"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../exampleapp.ui" line="98"/>
+        <source>labelInfoComp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../exampleapp.cpp" line="14"/>
+        <source>The application is build in standalone mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../exampleapp.cpp" line="14"/>
+        <source>The application is build for installation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../exampleapp.cpp" line="15"/>
+        <source>Compiled with %1 running on %2. Build Architecture is : %3, running Architecture is %4</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../Logs/sqlogging.cpp" line="57"/>
+        <source>Critical error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ ExampleApp/translations/exampleapp_fr.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>ExampleApp</name>
+    <message>
+        <location filename="../exampleapp.ui" line="14"/>
+        <source>ExampleApp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../exampleapp.ui" line="27"/>
+        <source>Debug Log Path : </source>
+        <translatorcomment>Chemin du journal de debug</translatorcomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../exampleapp.ui" line="40"/>
+        <source>Info Log Path :</source>
+        <translatorcomment>Chemin du journal d&apos;info</translatorcomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../exampleapp.ui" line="53"/>
+        <location filename="../exampleapp.ui" line="69"/>
+        <location filename="../exampleapp.ui" line="85"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../exampleapp.ui" line="98"/>
+        <source>labelInfoComp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../exampleapp.cpp" line="14"/>
+        <source>The application is build in standalone mode</source>
+        <translatorcomment>L&apos;application est construite en standalone</translatorcomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../exampleapp.cpp" line="14"/>
+        <source>The application is build for installation</source>
+        <translatorcomment>L&apos;application est construite pour l&apos;installation</translatorcomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../exampleapp.cpp" line="15"/>
+        <source>Compiled with %1 running on %2. Build Architecture is : %3, running Architecture is %4</source>
+        <translatorcomment>Compiled avec %1 tournant sous %2. Architecture de buid est : %3,Architecture d&apos;execution est %4</translatorcomment>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../Logs/sqlogging.cpp" line="57"/>
+        <source>Critical error</source>
+        <translation>Erreur critique</translation>
+    </message>
+</context>
+</TS>
