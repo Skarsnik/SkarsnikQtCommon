5e587baf5b46c241d36f26d4a4c408f801888310
Sylvain Colinet <scolinet@gmail.com>
2024-07-06 17:02:55 +0200

    Removed SQPackager from this repo

--- SQPackager/desktoprc.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <projectdefinition.h>
-
-bool    checkDesktopRC(const ProjectDefinition& proj, bool bypass = false);
-bool    generateLinuxDesktopRC(ProjectDefinition& proj);
-void    setDesktopRC(ProjectDefinition& proj);
-void    setIconSize(ProjectDefinition& proj);
--- SQPackager/github.h
+++
@@ -1,9 +0,0 @@
-#ifndef GITHUB_H
-#define GITHUB_H
-
-#include <QString>
-
-bool    isGithubAction();
-void    addGithubOutput(QString key, QString value);
-
-#endif // GITHUB_H
